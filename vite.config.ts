82c23fd1e663e120c3e0fc90c24c7f08558fe283
yakoobsenan <46875398+yakoobsenan@users.noreply.github.com>
2026-06-03 20:15:16 +0300

    Added tailwind.config.js

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,10 @@
-import tailwindcss from '@tailwindcss/vite';
 import react from '@vitejs/plugin-react';
 import path from 'path';
 import {defineConfig} from 'vite';
 
 export default defineConfig(() => {
   return {
-    plugins: [react(), tailwindcss()],
+    plugins: [react()],
     resolve: {
       alias: {
         '@': path.resolve(__dirname, '.'),
